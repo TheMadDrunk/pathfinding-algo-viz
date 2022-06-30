20c1cfe0f0d2d2b2583ece6884be5c557b32333f
unknown <iophamza2@gmail.com>
2022-06-30 02:24:18 +0100

    foo

--- include/searchAlgos.hpp
+++ include/searchAlgos.hpp
@@ -8,22 +8,20 @@
 #include "raylib.h"
 
 void TestNaiveAlgo(MatrixViz& mtx,int delay,bool& animationPlaying){
-    std::cout<<"here\n";
-    double LastT = GetTime();
     index2D idx = mtx.start;
     
     while (animationPlaying)
     {
-        std::this_thread::sleep_for (std::chrono::milliseconds((int)delay));
+        std::this_thread::sleep_for (std::chrono::milliseconds(delay));
         if(mtx.table[idx.i][idx.j].end)break;
         mtx.table[idx.i][idx.j].visited = true;
         std::cout<<"modified\n";
         if(idx.i<mtx.size-1)idx.i++;
         if(idx.j<mtx.size-1)idx.j++;
-        LastT = GetTime();
-        
     }
-    std::cout<<"here\n";
+    
 }
+
+
 
 #endif
